d903a3091e4debabb3cb14c94ddbed510b5a4f40
falseopx <62042556+falseopx@users.noreply.github.com>
2021-07-06 00:29:09 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,31 +1,16 @@
---detect if already executed or not--
-local VIVALibrary = Instance.new("ScreenGui")
-VIVALibrary.Name = "Lunar LuaL Engine"
-VIVALibrary.Parent = game:GetService("CoreGui")
-
-local detected = false
-
-for _, obj in pairs(game:GetService("CoreGui")) do
-  if obj:IsA("ScreenGui") and obj.Name = "Lunar LuaL Engine" then
-    detected = true
-  end
-end
+--thing--
+if getgenv().LUNARHUBLOADED then return; end
+pcall(function()
+    getgenv.LUNARHUBLOADED = true;
+end)
 --actual loader--
-if detected == true then
-  game.StarterGui:SetCore("SendNotification",{
-      Title = "Lunar Hub | Error";
-      Text = "You have already executed Lunar Hub, please rejoin to reexecute.";
-      Icon = "";
-      Duration = 5;
-  })
-elseif detected == false then
-  local games = {
-    [286090429] = "Arsenal"
-  }
+local games = {
+    [286090429] = "arsenal"
+}
 
-  for pid, url in pairs(games) do
+for pid, url in pairs(games) do
     if game.PlaceId == pid then
-       loadstring(game:HttpGet("https://raw.githubusercontent.com/L-U-N-A-R/Lunar-Hub/main/Games/".. url ..".lua"))()
+        print("dababy")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/L-U-N-A-R/Lunar-Hub/main/games/".. url ..".lua"))()
     end
-  end
 end
