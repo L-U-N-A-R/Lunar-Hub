d28bfa38605dd8786d941cf07ab446015fccbe4e
falseopx <62042556+falseopx@users.noreply.github.com>
2021-07-05 23:57:54 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,20 +1,20 @@
 --detect if already executed or not--
 local VIVALibrary = Instance.new("ScreenGui")
-VIVALibrary.Name = "VIVA LuaV Engine"
+VIVALibrary.Name = "Lunar LuaL Engine"
 VIVALibrary.Parent = game:GetService("CoreGui")
 
 local detected = false
 
 for _, obj in pairs(game:GetService("CoreGui")) do
-  if obj:IsA("ScreenGui") and obj.Name = "VIVA LuaV Engine" then
+  if obj:IsA("ScreenGui") and obj.Name = "Lunar LuaL Engine" then
     detected = true
   end
 end
 --actual loader--
 if detected == true then
   game.StarterGui:SetCore("SendNotification",{
-      Title = "VIVA Hub | Error";
-      Text = "You have already executed VIVA Hub, please rejoin to reexecute.";
+      Title = "Lunar Hub | Error";
+      Text = "You have already executed Lunar Hub, please rejoin to reexecute.";
       Icon = "";
       Duration = 5;
   })
@@ -25,7 +25,7 @@ elseif detected == false then
 
   for pid, url in pairs(games) do
     if game.PlaceId == pid then
-       loadstring(game:HttpGet("https://raw.githubusercontent.com/V-I-V-A/viva-hub-r2/main/Games/".. url ..".lua"))()
+       loadstring(game:HttpGet("https://raw.githubusercontent.com/L-U-N-A-R/Lunar-Hub/main/Games/".. url ..".lua"))()
     end
   end
 end
